1824d9f7627af205ce841d949b85b7c688f6183f
ahdeshpande <deshpande.abhiraj@gmail.com>
2018-10-20 15:14:06 +0000

    feat: Create ItemsList component

--- ControlledComponents/src/App.js
+++ ControlledComponents/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import ItemsList from './components/ItemsList';
 
 class App extends React.Component {
   state = {
@@ -53,10 +54,7 @@ class App extends React.Component {
           Delete Last Item
         </button>
 
-        <p className="items">Items</p>
-        <ol className="item-list">
-          {this.state.items.map((item, index) => <li key={index}>{item}</li>)}
-        </ol>
+        <ItemsList items={this.state.items}/>
       </div>
     );
   }
