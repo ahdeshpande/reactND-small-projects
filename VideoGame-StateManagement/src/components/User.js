4f703910847bd9790994cdb0e28be275a8a5d42b
ahdeshpande <deshpande.abhiraj@gmail.com>
2018-10-20 14:09:11 -0400

    feat: Create list component

--- VideoGame-StateManagement/src/components/User.js
+++ VideoGame-StateManagement/src/components/User.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const User = props => {
-    return <li>{props.username} played {props.showGamesPlayed ? props.gameCount : '*'} {props.gameCount === 1 ? 'game.' : 'games.'}</li>
+    return <li>{props.user.username} played {props.showGamesPlayed ? props.user.gameCount : '*'} {props.user.gameCount === 1 ? 'game.' : 'games.'}</li>
 };
 
 export default User;
---
+++ VideoGame-StateManagement/src/components/UsersList.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import User from "./User";
+
+export default class UsersList extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {showGamesPlayed: true,};
+    }
+
+    handleClick = () => {
+        this.setState(prevState => {
+            return {showGamesPlayed: !prevState.showGamesPlayed}
+        });
+    };
+
+    render() {
+        const {userlist} = this.props;
+        const {showGamesPlayed} = this.state;
+        return (
+            <div>
+                <h3>Users
+                    <button style={{
+                        float: 'right',
+                    }} onClick={this.handleClick}>
+                        {showGamesPlayed ? 'Hide' : 'Show'} games played
+                    </button>
+                </h3>
+                <ol>
+                    {userlist.map(user => (
+                        <User user={user} key={user.username}
+                              showGamesPlayed={showGamesPlayed}/>
+                    ))}
+                </ol>
+            </div>
+        )
+    }
+
+}
